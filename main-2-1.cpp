892993837706506cddd5adc2e36b81e368bc374f
Cuinn Kemp <cuinn.kemp@student.adelaide.edu.au>
2022-07-27 12:06:29 +0930

    Q2-1 completed

---
+++ main-2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+extern int min_element(int[], int);
+
+int main(){
+    int array[5] = {6,2,3,4,5}; int n = 5;
+    int sum = min_element(array, n);
+    printf("%d\n", sum);
+}
