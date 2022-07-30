4ea2605b5a320473abd821ec35fea90f43e21119
Cuinn Kemp <cuinn.kemp@student.adelaide.edu.au>
2022-07-30 23:16:04 +0930

    Q2-4 completed

---
+++ function-2-4.cpp
@@ -0,0 +1,32 @@
+
+int array_min(int integers[], int length){
+    if (length <= 0){ return -1;}
+    int min = integers[0];
+    for (int i = 1; i < length; i++){
+        if (integers[i] < min){
+            min = integers[i];
+        }
+    }
+    return min;
+}
+
+int array_max(int integers[], int length){
+    if (length <= 0){ return -1;}
+    int max = integers[0];
+    for (int i = 1; i < length; i++){
+        if (integers[i] > max){
+            max = integers[i];
+        }
+    }
+    return max;
+}
+
+int sum_min_max(int integers[], int length){
+    if (length <= 0){ return -1;}
+    int max = array_max(integers, length);
+    int min = array_min(integers, length);
+
+    int sum = max + min;
+
+    return sum;
+}
