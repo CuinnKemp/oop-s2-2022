bbe2efe67aa9712ef215143a4f067d518112997b
Cuinn Kemp <cuinn.kemp@student.adelaide.edu.au>
2022-07-30 22:15:32 +0930

    Q1-4 completed

---
+++ function-1-4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_scaled(int array[3][3],int scale){
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            array[i][j] = array[i][j] * scale;
+            printf("%d ",array[i][j]);
+        }
+        printf("\n");
+    }
+}
