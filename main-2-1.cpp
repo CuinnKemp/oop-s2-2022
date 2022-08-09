67a076019893c06b87f9360986a099b98122ae09
Cuinn Kemp <cuinn.kemp@student.adelaide.edu.au>
2022-08-09 14:26:07 +0930

    set up workshop

--- main-2-1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-extern void print_binary_str(std::string);
-
-int main(){
-    std::string number = "300";
-    print_binary_str(number);
-}
--- main-2-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-extern int binary_to_int(int[], int);
-
-int main(){
-    int binary_array[9] = {1,0,0,1,0,1,1,0,0}; int length = 9;
-    int decNum = binary_to_int(binary_array, length);
-    printf("%d\n", decNum);
-}
