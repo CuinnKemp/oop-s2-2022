b4029144538d44c935067e5f453fa61267250c9b
Cuinn Kemp <cuinn.kemp@student.adelaide.edu.au>
2022-08-09 15:12:04 +0930

    bug fixes

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 void print_class(std::string courses[4], std::string students[], int report_card[][4], int nstudents){
-    for (int i = 0; i < nstudents; i++){
-        cout << "Report Card" << " ";
-        for (int j = 0; j < 4; j++){
-            cout << courses[j];
-            if (j != 3){
-                cout << " ";
-            }
+    cout << "Report Card" << " ";
+    for (int j = 0; j < 4; j++){
+        cout << courses[j];
+        if (j != 3){
+            cout << " ";
         }
-        cout << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < nstudents; i++){
         cout << students[i] << " ";
         for (int j = 0; j < 4; j++){
             cout << report_card[i][j];
