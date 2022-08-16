7e147597f005b452a54ea58398f24c272ba09f69
Cuinn Kemp <cuinn.kemp@student.adelaide.edu.au>
2022-08-16 15:14:58 +0930

    tinkered

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <sys/ioctl.h>
+#include <iostream>
+using namespace std;
+#include <unistd.h>
+
+
+int main (void)
+{
+   
+    char item;    
+    cin.get(item, 25);
+    cout << item;
+
+        
+}
