44cc1c16d7ff81b5a2ba83cb9dccb0d66af3d896
Cuinn Kemp <cuinn.kemp@student.adelaide.edu.au>
2022-08-17 16:42:02 +0930

    testing completed

---
+++ test.cpp
@@ -0,0 +1,70 @@
+#include <cmath>
+#include <sys/ioctl.h>
+#include <iostream>
+using namespace std;
+#include <unistd.h>
+#define pi = 3.14159265;
+
+class Screen{
+    private:
+        double directionVector1[3] = {cos(angle),sin(angle),0};
+        double directionVector2[3] = {0,0,1};
+        double normalVector[3] = {sin(angle), -cos(angle), 0};
+        double angle = 0;
+    public:
+        void set_angle(double new_angle){
+            angle = new_angle;
+            normalVector[0] = sin(angle);
+            normalVector[1] = -cos(angle);
+            directionVector1[0] = cos(angle);
+            directionVector1[1] = sin(angle);
+        }
+
+        double* findPointOnPlane(double point[3]){
+            double* pointOnPlane = new double[3];
+            for (int i = 0; i < 3; i++){
+                pointOnPlane[i] = -9999999;
+            }
+            double lambda = ((-point[0])*sin(angle)+point[1]*cos(angle))/(-pow(sin(angle),2)-pow(cos(angle),2));
+            if (lambda < 0){
+                return pointOnPlane;
+            }
+            for (int i = 0; i < 3; i++){
+                pointOnPlane[i] = point[i] - (lambda * normalVector[i]);
+            }
+
+            return pointOnPlane;
+        }
+
+};
+
+int main(){
+    int cols = 80;
+    int lines = 24;
+
+    #ifdef TIOCGSIZE
+        struct ttysize ts;
+        ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
+        cols = ts.ts_cols;
+        lines = ts.ts_lines;
+    #elif defined(TIOCGWINSZ)
+        struct winsize ts;
+        ioctl(STDIN_FILENO, TIOCGWINSZ, &ts);
+        cols = ts.ws_col;
+        lines = ts.ws_row;
+    #endif /* TIOCGSIZE */
+
+    int array[lines][cols] = {0};
+
+    Screen Screen1;
+
+    double point[3] = {1,2,3};
+    double* pointOnPlane = Screen1.findPointOnPlane(point);
+    cout << pointOnPlane[0]<< " " << pointOnPlane[1] << " " << pointOnPlane[2] << endl;
+    for (int i = 0; i < 3; i++){
+        pointOnPlane[i] = round(pointOnPlane[i]);
+    }
+    
+    
+    delete[] pointOnPlane;
+}
